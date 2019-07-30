4ddd9fe3fb2172463a28fb465896d8f1eb8caf70
Luis Boscan <luife642@gmail.com>
2019-07-30 00:19:13 -0400

    Minor fix in Fib js file

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -13,17 +13,17 @@ class Fib extends Component {
     this.fetchIndexes();
   }
 
-  async fetchValues() {
+  fetchValues = async () => {
     const values = await axios.get('/api/values/current');
     this.setState({ values: values.data });
-  }
+  };
 
-  async fetchIndexes() {
+  fetchIndexes = async () => {
     const seenIndexes = await axios.get('/api/values/all');
     this.setState({
       seenIndexes: seenIndexes.data
     });
-  }
+  };
 
   handleSubmit = async event => {
     event.preventDefault();
